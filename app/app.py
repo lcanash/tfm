684199841775f17a093756b9c1930477d932dcd7
lcanash <121309863+lcanash@users.noreply.github.com>
2022-12-23 18:45:05 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
-
+from sklearn.metrics import precision_score, recall_score 
 
 header = st.container()
 dataset = st.container()
