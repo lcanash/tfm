4f4d3072f834381e20a33704bfac123173d7f3bd
lcanash <121309863+lcanash@users.noreply.github.com>
2022-12-23 18:52:12 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import sklearn as sk
-import scikitplot as skplt
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
