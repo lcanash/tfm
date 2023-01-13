29402610efc4f1834c5cf69ed2c6b9eca1534725
lcanash <121309863+lcanash@users.noreply.github.com>
2023-01-13 01:56:29 +0100

    Update app.py

--- app/app.py
+++ app/app.py
@@ -17,7 +17,7 @@ with header:
 	st.title("Application to detect samples with lung cancer from microarray datasets")
 
 with example:
-	DATA_URL = ("./app/samplet.csv")
+	DATA_URL = ("./app/sample.csv")
 	example = pd.read_csv(DATA_URL)
 	st.write(example)
 	
